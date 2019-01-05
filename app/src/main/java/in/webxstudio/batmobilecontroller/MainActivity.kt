2fc22cf1b510c7ec92b81bac114c1d28a1c1bdde
gat786 <ganesht049@gmail.com>
2019-01-06 01:26:05 +0530

    exoplayer dash media source changed

--- app/src/main/java/in/webxstudio/batmobilecontroller/MainActivity.kt
+++ app/src/main/java/in/webxstudio/batmobilecontroller/MainActivity.kt
@@ -1,36 +1,28 @@
 package `in`.webxstudio.batmobilecontroller
 
-import android.app.Activity
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.support.v4.view.GestureDetectorCompat
-import android.util.Log
-import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
 import com.google.android.exoplayer2.*
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
-import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.dash.DashMediaSource
 import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
-import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
-import java.util.*
 
 class MainActivity : AppCompatActivity(), AnkoLogger
 {
-    val videoUrl = "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8"
+    val videoUrl = "http://192.168.137.240:8160/"
 
     private lateinit var player:SimpleExoPlayer
     val mHandler = Handler()
@@ -45,14 +37,45 @@ class MainActivity : AppCompatActivity(), AnkoLogger
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         initializePlayer()
+
+        up_button.setOnTouchListener { v, event ->
+            onTouchEvent(event,Direction.Forward,v)
+        }
+
+        down_button.setOnTouchListener { v, event ->
+            onTouchEvent(event,Direction.Backward,v)
+        }
+
+        left_button.setOnTouchListener { v, event ->
+            onTouchEvent(event)
+        }
+
+    }
+    fun onTouchEvent(
+        event: MotionEvent?,
+        direction: Direction? = null,
+        view: View): Boolean {
+        if (direction==Direction.Forward){
+            info { "move ahead" }
+            view.elevation = Integer.parseInt("4").toFloat()
+        }
+        else if (direction == Direction.Backward){
+            info { "move backwards" }
+        }
+        else if (direction == Direction.Right){
+            info { "increasing angle towards right" }
+        }
+        else if (direction == Direction.Left){
+            info { "increasing angle towards left" }
+        }
+        return true
     }
 
     fun initializePlayer(){
         player = ExoPlayerFactory.newSimpleInstance(baseContext)
         video_view.player = player
-        //player.prepare(createDataSource(videoUrl))
+        player.prepare(getHlsMediaSource(videoUrl))
     }
 
     fun createDataSource(videoUrl:String): DashMediaSource? {
@@ -72,9 +95,20 @@ class MainActivity : AppCompatActivity(), AnkoLogger
         return dashMediaSource.createMediaSource(Uri.parse(videoUrl))
     }
 
+    fun getHlsMediaSource(hls: String): MediaSource {
+        val bandwidthMeter = DefaultBandwidthMeter()
+        val applicationName = Util.getUserAgent(baseContext, "BatMobile")
+        val dataSourceFactory = DefaultDataSourceFactory(baseContext, applicationName, bandwidthMeter)
+        val factory = HlsMediaSource.Factory(dataSourceFactory)
+        return factory.createMediaSource(Uri.parse(hls))
+    }
+
     fun pauseListener(){
         val listener  = object: Player.EventListener{
 
         }
     }
+}
+enum class Direction{
+    Forward,Backward,Right,Left
 }
